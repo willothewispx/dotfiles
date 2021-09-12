30b362302670c73a340fb00c72822340350f67a1
Denes Stolte <contact@stolte.me>
2021-09-12 14:47:29 +0200

    Diffview integration in neogit

--- nvim/.config/nvim/after/plugin/neogit.lua
+++ nvim/.config/nvim/after/plugin/neogit.lua
@@ -1,4 +1,22 @@
 require('neogit').setup{
   disable_signs = false,
   disable_context_highlighting = false,
+  disable_commit_confirmation = false,
+  auto_refresh = true,
+  disable_builtin_notifications = false,
+  commit_popup = {
+    kind = "split",
+  },
+  -- customize displayed signs
+  signs = {
+    -- { CLOSED, OPENED }
+    section = { ">", "v" },
+    item = { ">", "v" },
+    hunk = { "", "" },
+  },
+  integrations = {
+    -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
+    -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
+    diffview = true
+  },
 }
