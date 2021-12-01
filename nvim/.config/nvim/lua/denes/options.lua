6f451a860335550ac1b2b4cc9ab3546d11649869
Denes Stolte <contact@stolte.me>
2021-12-01 22:16:10 +0100

    stylua format

--- nvim/.config/nvim/lua/denes/options.lua
+++ nvim/.config/nvim/lua/denes/options.lua
@@ -24,8 +24,8 @@ opt.hlsearch = false
 opt.cursorline = true
 
 opt.textwidth = 80
-opt.colorcolumn = '+1'
-opt.signcolumn = 'yes'
+opt.colorcolumn = "+1"
+opt.signcolumn = "yes"
 
 opt.breakindent = true
 opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
@@ -33,30 +33,30 @@ opt.linebreak = true
 
 opt.scrolloff = 10
 
-opt.inccommand = 'split'
+opt.inccommand = "split"
 
 opt.swapfile = false
 
-local home = os.getenv('HOME')
-opt.undodir = home .. '/.nvim/undodir'
+local home = os.getenv("HOME")
+opt.undodir = home .. "/.nvim/undodir"
 opt.undofile = true
 
-opt.mouse = 'n'
+opt.mouse = "n"
 
 opt.updatetime = 1000
 
-opt.completeopt = 'menuone,noselect'
+opt.completeopt = "menuone,noselect"
 
 opt.formatoptions = opt.formatoptions
-  - "a" -- Auto formatting is BAD.
-  - "t" -- Don't auto format my code. I got linters for that.
-  + "c" -- In general, I like it when comments respect textwidth
-  + "q" -- Allow formatting comments w/ gq
-  - "o" -- O and o, don't continue comments
-  + "r" -- But do continue when pressing enter.
-  + "n" -- Indent past the formatlistpat, not underneath it.
-  + "j" -- Auto-remove comments if possible.
-  - "2" -- I'm not in gradeschool anymore
+    - "a" -- Auto formatting is BAD.
+    - "t" -- Don't auto format my code. I got linters for that.
+    + "c" -- In general, I like it when comments respect textwidth
+    + "q" -- Allow formatting comments w/ gq
+    - "o" -- O and o, don't continue comments
+    + "r" -- But do continue when pressing enter.
+    + "n" -- Indent past the formatlistpat, not underneath it.
+    + "j" -- Auto-remove comments if possible.
+    - "2" -- I'm not in gradeschool anymore
 
 -- set joinspaces
 opt.joinspaces = false -- Two spaces and grade school, we're done
