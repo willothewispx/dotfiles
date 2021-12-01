a2aea26813677ffe1323097409bf0f1ccbfbab79
Denes Stolte <contact@stolte.me>
2021-12-01 22:15:41 +0100

    null-ls:stylua: use spaces instead of tabs

--- nvim/.config/nvim/lua/denes/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/denes/lsp/null-ls.lua
@@ -2,7 +2,10 @@ local null_ls = require("null-ls")
 
 local sources = {
     null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.formatting.stylua
+
+    null_ls.builtins.formatting.stylua.with({
+        extra_args = {"--indent-type", "Spaces"}
+    })
 }
 
 null_ls.config({sources = sources})
