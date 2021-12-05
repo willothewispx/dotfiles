a5d78fe62d085543158ec604c507a9bcb1855d0c
Denes Stolte <contact@stolte.me>
2021-12-05 15:34:22 +0100

    null-ls: lua formatter

--- nvim/.config/nvim/lua/denes/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/denes/lsp/null-ls.lua
@@ -1,16 +1,19 @@
 local null_ls = require("null-ls")
 
 local sources = {
+    -- python
     null_ls.builtins.formatting.autopep8,
+    null_ls.builtins.diagnostics.flake8,
 
+    -- lua
     null_ls.builtins.formatting.stylua.with({
-        extra_args = {"--indent-type", "Spaces"}
-    })
+        extra_args = { "--indent-type", "Spaces" },
+    }),
 }
 
-null_ls.config({sources = sources})
+null_ls.config({ sources = sources })
 
 require("lspconfig")["null-ls"].setup({
     -- see the nvim-lspconfig documentation for available configuration options
-    on_attach = require("denes.lsp.options").custom_attach
+    on_attach = require("denes.lsp.options").custom_attach,
 })
