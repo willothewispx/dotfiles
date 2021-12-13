9379284b453355a29776733736919ef48a5d35d0
Denes Stolte <contact@stolte.me>
2021-12-14 00:40:28 +0100

    nvim: luasnip with twig extension

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -1,5 +1,8 @@
-require'luasnip'.config.set_config({
-  history = true,
-  updateevents = "TextChanged,TextChangedI"
+require("luasnip").config.set_config({
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
 })
+
 require("luasnip.loaders.from_vscode").load()
+
+require("luasnip").filetype_extend("php", { "twig" })
