b4982d54b979b6434705b4457eeaeff59bd40adf
Denes Stolte <contact@stolte.me>
2021-12-14 00:28:03 +0100

    nvim: transparent sidebar

--- nvim/.config/nvim/lua/denes/colors/tokyonight.lua
+++ nvim/.config/nvim/lua/denes/colors/tokyonight.lua
@@ -4,7 +4,7 @@ vim.g.tokyonight_style = "night"
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
 -- vim.g.tokyonight_transparent = true
--- vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent_sidebar = true
 
 vim.cmd("colorscheme tokyonight")
 
