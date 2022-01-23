38b77c61dbaec19b22561dec9ef1865136951763
Denes Stolte <contact@stolte.me>
2022-01-23 13:27:54 +0100

    nvim: Manage notes in neorg.

--- nvim/.config/nvim/after/plugin/neorg.lua
+++ nvim/.config/nvim/after/plugin/neorg.lua
@@ -6,7 +6,8 @@ require("neorg").setup({
         ["core.norg.dirman"] = { -- Manage your directories with Neorg
             config = {
                 workspaces = {
-                    personal = "~/Nextcloud/Neorg",
+                    personal = "~/Nextcloud/Neorg/personal",
+                    huong = "~/Nextcloud/Neorg/huong",
                 },
                 -- Automatically detect whenever we have entered a subdirectory of a workspace
                 autodetect = true,
@@ -22,6 +23,30 @@ require("neorg").setup({
         ["core.integrations.telescope"] = {}, -- Enable the telescope module
     },
 })
+
+-- This sets the leader for all Neorg keybinds. It is separate from the regular <Leader>,
+-- And allows you to shove every Neorg keybind under one "umbrella".
+local neorg_leader = "<Leader>" -- You may also want to set this to <Leader>o for "organization"
+
+-- Require the user callbacks module, which allows us to tap into the core of Neorg
+local neorg_callbacks = require("neorg.callbacks")
+
+-- Listen for the enable_keybinds event, which signals a "ready" state meaning we can bind keys.
+-- This hook will be called several times, e.g. whenever the Neorg Mode changes or an event that
+-- needs to reevaluate all the bound keys is invoked
+neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+    -- Map all the below keybinds only when the "norg" mode is active
+    keybinds.map_event_to_mode("norg", {
+        n = { -- Bind keys in normal mode
+
+            -- Keys for managing TODO items and setting their states
+            { "gtd", "core.norg.qol.todo_items.todo.task_done" },
+            { "gtu", "core.norg.qol.todo_items.todo.task_undone" },
+            { "gtp", "core.norg.qol.todo_items.todo.task_pending" },
+            { "<C-Space>", "core.norg.qol.todo_items.todo.task_cycle" },
+        },
+    }, { silent = true, noremap = true })
+end)
 
 -- Get the current Neorg state
 local neorg = require("neorg")
