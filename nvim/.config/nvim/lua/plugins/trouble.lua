24b251df089f1c32dcdbe6fb1c7106fa4734e7a7
Denes <contact@stolte.me>
2026-04-05 21:47:41 +0200

    feat(nvim): enhance LSP key mappings for quickfix and Trouble integration

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -12,6 +12,8 @@ return {
     { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location list (Trouble)" },
     { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix list (Trouble)" },
     { "<leader>xr", "<cmd>Trouble lsp_references toggle focus=false win.position=bottom<cr>", desc = "References (Trouble)" },
+    { "<leader>xi", "<cmd>Trouble lsp_implementations toggle focus=false win.position=bottom<cr>", desc = "Implementations (Trouble)" },
+    { "<leader>xR", "<cmd>Trouble lsp_references focus<cr>", desc = "Focus references (Trouble)" },
   },
   opts = {
     focus = false,
